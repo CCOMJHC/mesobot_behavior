33d13450b6da9f1546facc7add003d223d8823a2
Val Schmidt <vschmidt@ccom.unh.edu>
2023-09-16 01:22:19 +0000

    Added debugging input from blackboard. Feedback mechanism.

--- src/mesobot_emergency_condition.py
+++ src/mesobot_emergency_condition.py
@@ -1,24 +1,35 @@
+import rospy
 import py_trees
 from mesobot_blackboard import bhv_bb
+from project11_msgs.msg import BehaviorInformation
 
 # Emergnecy Condition
 class Emergency(py_trees.behaviour.Behaviour):
-    def __init__(self,name):
+    def __init__(self,name,feedback):
         super(Emergency,self).__init__(name)
         self.blackboard = bhv_bb
-        self.emergency_status = False
+        self.feedback_pub = feedback
+        self.is_emergency = False
 
     def setup(self,timeout):
         '''How do we change the emergency status?'''
+        feedback = BehaviorInformation()
+        feedback.data = "Setup Emergency Condition."
+        self.feedback_pub.publish(feedback)
+
         return True
     
     def initialise(self):
         pass
+
     def update(self):
-        if self.emergency_status:
+        if self.blackboard.get('test.debug'):
+            self.is_emergency = self.blackboard.get('test.emergency')
+        if self.is_emergency:
+            self.logger.debug("  %s [Emergency::update()]" % self.name)
+            rospy.loginfo("Mesobot Emergency")
             return py_trees.common.Status.SUCCESS
         else:
-            self.logger.debug("  %s [Emergency::update()]" % self.name)
             return py_trees.common.Status.FAILURE
     def terminate(self,new_status):
         pass
